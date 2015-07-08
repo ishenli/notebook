a2dfa2ac267931e677c979661652c067a78ba724
shenli <meshenli@gmail.com>
2015-07-08 14:54:06 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,6 @@ var rider = require('rider');
 var stylus = require('gulp-stylus');
 var postcss = require('gulp-postcss');
 
-/*var tpl2js = require('gulp-tpl2js');
-
-gulp.task('html2js', function () {
-    return gulp.src('src/!**!/!*.tpl', {base: 'src'})
-        .pipe(tpl2js({
-            type: 'amd'
-        }))
-        .pipe(gulp.dest('src'));
-});*/
-
 gulp.task('stylus', function(){
     var processors = [
         require('autoprefixer')('last 1 version'),
@@ -27,6 +17,5 @@ gulp.task('stylus', function(){
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/**/*.tpl', ['html2js']);
     gulp.watch('src/**/**.styl', ['stylus']);
 });
